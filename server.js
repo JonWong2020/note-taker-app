e8dabfe41a1aee769b9e719065ec8ef8b0564f78
JonWong2020 <jonathannwong50@aol.com>
2022-09-18 20:53:46 -0700

    added POST requests

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // libs to import
 const express = require('express');
 const fs = require('fs');
+const { request } = require('http');
 const path = require('path');
 
 // server ports
@@ -16,17 +17,39 @@ app.use(express.static('public'));
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'), err => {
         if (err) throw err;
-    })
-})
+    });
+});
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'), err =>{
         if (err) throw err;
-    })
-})
+    });
+});
 
 app.get('/api/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'db/db.json'), err => {
         if (err) throw err;
-    })
-})
+    });
+});
+
+// POST request functions
+app.post('/api/notes', (req, res) => {
+    const newNoteData = req.body; 
+
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) throw err; 
+        const updatedNoteData = JSON.parse(data);
+        updatedNoteData.push(newNoteData);
+
+        fs.writeFile('./db/db.json', JSON.stringify(updatedNoteData), err => {
+            if (err) throw err;
+        });
+    });
+
+    res.sendStatus(200);
+
+});
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
