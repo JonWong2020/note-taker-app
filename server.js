9bf9057a113fb85b4d22e339c3e30fbbfa642623
JonWong2020 <jonathannwong50@aol.com>
2022-09-18 20:46:16 -0700

    added GET requests

--- server.js
+++ server.js
@@ -8,8 +8,25 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 // app staging
-app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 
 // GET Request functions
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'), err => {
+        if (err) throw err;
+    })
+})
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'), err =>{
+        if (err) throw err;
+    })
+})
+
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'db/db.json'), err => {
+        if (err) throw err;
+    })
+})
